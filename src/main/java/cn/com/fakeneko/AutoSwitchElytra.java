6e6c9dae443784369ec88b53e88d85512fca200c
“nocturne” <2014746403@qq.com>
2024-08-12 20:01:12 +0800

    客户端启动成功：gradle -》Tasks -〉fabric -》runClient

--- src/main/java/cn/com/fakeneko/AutoSwitchElytra.java
+++ src/main/java/cn/com/fakeneko/AutoSwitchElytra.java
@@ -9,7 +9,9 @@ public class AutoSwitchElytra implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("auto-switch-elytra");
+	public static final String MOD_ID = "auto_switch_elytra";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/cn/com/fakeneko/AutoSwitchElytraClient.java
@@ -0,0 +1,15 @@
+package cn.com.fakeneko;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author nocturne
+ * @date 2024/8/12 下午7:46
+ * @description
+ */
+public class AutoSwitchElytraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
