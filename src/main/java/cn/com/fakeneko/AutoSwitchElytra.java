6056341785030a4132e64b16f4a7be9a3fa4a298
fakeneko <jdong6639@163.com>
2024-08-17 15:56:05 +0800

    修改modid，并且添加物品、方块初始化

--- src/main/java/cn/com/fakeneko/AutoSwitchElytra.java
+++ src/main/java/cn/com/fakeneko/AutoSwitchElytra.java
@@ -1,6 +1,8 @@
 package cn.com.fakeneko;
 
+import cn.com.fakeneko.block.BlockDemo;
 import net.fabricmc.api.ModInitializer;
+import cn.com.fakeneko.item.ModItemsDemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +11,7 @@ public class AutoSwitchElytra implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "auto_switch_elytra";
+	public static final String MOD_ID = "auto-switch-elytra";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -19,6 +21,9 @@ public class AutoSwitchElytra implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		// 初始化物品
+		ModItemsDemo.onItemInitialize();
+		BlockDemo.onBlockInitialize();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
