bde6be950c40f79a63b8f9b6cf33a44c22fe3c27
fakeneko <jdong6639@163.com>
2024-08-17 15:57:42 +0800

    模组物品样例

---
+++ src/main/java/cn/com/fakeneko/item/ModItemsDemo.java
@@ -0,0 +1,36 @@
+package cn.com.fakeneko.item;
+
+import cn.com.fakeneko.AutoSwitchElytra;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+/**
+ * @author fakeneko
+ * @date 2024/8/17下午1:46
+ * @description
+ */
+public class ModItemsDemo {
+    // 注册一个物品
+
+    // 物品ID声明
+    public static final Item ICE_ETHER = new Item(new Item.Settings());
+    public static final Item FIRE_ETHER = new Item(new Item.Settings());
+    public static final Item EXAMPLE_BLOCK = new Item(new Item.Settings());
+
+    // 物品注册函数
+    private static void ModItemsDemoRegistry(String lowercase_item_name, Item item_name) {
+        Registry.register(Registries.ITEM, Identifier.of(AutoSwitchElytra.MOD_ID, lowercase_item_name), item_name);
+    }
+
+    // 实际物品注册
+    public static void onItemInitialize() {
+        AutoSwitchElytra.LOGGER.info("Registry Item Start!");
+        ModItemsDemoRegistry("ice_ether", ICE_ETHER);
+        ModItemsDemoRegistry("fire_ether", FIRE_ETHER);
+        ModItemsDemoRegistry("example_block", EXAMPLE_BLOCK);
+        AutoSwitchElytra.LOGGER.info("Registry Item End!");
+    }
+}
